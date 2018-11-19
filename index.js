9c48199ab3df6eec7fc298e556c98dc10c761a75
Tali Scheer <tali.scheer@gmail.com>
2018-11-19 20:27:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(line, name) {
+        line.push(name);
+ return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+  
+ 
+
+function nowServing(line) {
+var str = line[0];
+ if (line.length === 0) {
+    return `There is nobody waiting to be served!`;
+} else {
+   line.shift();
+   return `Currently serving ${str}.`;
+}
+}
+ 
+ 
+function currentLine(katzDeliLine) {
+for (var i = 0; i < katzDeliLine.length; i++) {
+      return `The line is currently${ i === 0 ? ' empty.' : ''}`;
+}
+}
