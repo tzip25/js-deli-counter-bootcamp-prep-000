b2ecee6e97ef804b80910ede5655ca679761fe72
Tali Scheer <tali.scheer@gmail.com>
2018-11-19 21:23:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ for (var i = 0; i < line.length; i++) {
 if (line.length === 0) {
       return `The line is currently empty.`;
 } else {
-  return (`The line is currently:` + newLine);
+  return (`The line is currently: ` + newLine);
 }
 }
 
