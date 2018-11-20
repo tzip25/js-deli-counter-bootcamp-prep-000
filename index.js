e58734052fc6513d7f902999fcfd3905a4418357
Tali Scheer <tali.scheer@gmail.com>
2018-11-20 01:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function nowServing(line) {
  if (line.length === 0) {
     return `There is nobody waiting to be served!`;
 } else {
-    var str = line[0];
    line.shift(0);
+    var str = line[0];
    return `Currently serving ${str}.`;
 }
 }
